40ea780f27faec421decc8479a5d0efae5a1d98c
Douglas Camata <d.camata@gmail.com>
2011-10-27 14:50:34 -0200

    pasta trunk deletada

--- trunk/nsi/multimedia/utils.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-
-def replace_file_extension(filename, extension):
-    dot = filename.rfind('.')
-    new_filename = filename[:dot] + '.' + extension
-    return new_filename
-
