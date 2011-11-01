0e0ef4c2eda43d3e4c6f1634b70d5b6e0cd30528
Felipe Norato Lacerda <felipe.norato@iff.edu.br>
2011-11-01 14:03:22 -0700

    teste converter mpeg para ogv

--- nsi/multimedia/tests/test_convert_all_video_formats.py
+++ nsi/multimedia/tests/test_convert_all_video_formats.py
@@ -3,19 +3,28 @@ from should_dsl import should
 import os
 
 from nsi.multimedia.transform.flv_converter import FlvConverter
+from nsi.multimedia.transform.ogg_converter import OggConverter
 
 class TestConvertVideoFormat(unittest.TestCase):
 
     def setUp(self):
         self.input_file = "inputs/cachorros.mpg"
-        self.output = "output/test.flv"
+        self.output_flv = "output/test.flv"
+        self.output_ogv = "output/test.ogv"
 
-    def test_avi_to_flv(self):
-        conversor = FlvConverter(source=self.input_file, target=self.output)
+    def test_mpg_to_flv(self):
+        conversor = FlvConverter(source=self.input_file, target=self.output_flv)
         conversor.run()
-        size_of_output_file = os.path.getsize(self.output)
+        size_of_output_file = os.path.getsize(self.output_flv)
         size_of_output_file |should| be_greater_than(0)
-        os.system("rm %s"%(self.output))
+        os.system("rm %s"%(self.output_flv))
+
+    def test_mpg_to_ogv(self):
+        conversor = OggConverter(source=self.input_file, target=self.output_ogv)
+        conversor.run()
+        size_of_output_file = os.path.getsize(self.output_ogv)
+        size_of_output_file |should| be_greater_than(0)
+        os.system("rm %s"%(self.output_ogv))
 
 
 
