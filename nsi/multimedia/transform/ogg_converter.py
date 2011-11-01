35bfc6da36afced3af45f205a7a1844a64100cb8
Douglas Camata <d.camata@gmail.com>
2011-11-01 00:48:43 -0200

    herdando do conversor base

--- nsi/multimedia/transform/ogg_converter.py
+++ nsi/multimedia/transform/ogg_converter.py
@@ -1,8 +1,8 @@
 ##############################################################################
 #
-# Copyright (c) 2009 ISrg (NSI, IFF, BRAZIL) and Contributors. 
+# Copyright (c) 2009 ISrg (NSI, IFF, BRAZIL) and Contributors.
 #                                                     All Rights Reserved.
-#                     Rodrigo S. Manhaes <rmanhaes@gmail.com> 
+#                     Rodrigo S. Manhaes <rmanhaes@gmail.com>
 #
 # WARNING: This program as such is intended to be used by professional
 # programmers who take the whole responsability of assessing all potential
@@ -32,26 +32,14 @@ import logging
 import sys
 from subprocess import Popen
 
+from nsi.multimedia.transform.base_converter import BaseConverter
 from nsi.multimedia.utils import replace_file_extension
 
-class OggConverter:
+class OggConverter(BaseConverter):
     '''
-    Converts a video input file to OGM. We elected OGM as our canonical video format. 
+    Converts a video input file to OGM. We elected OGM as our canonical video format.
     '''
-    
-    def __init__(self, source, target=None, log_stream=sys.stdout):
-        '''
-        source is the input file (not altered)
-        target is the output file (created or overriden)
-        '''
-        logging.basicConfig(level=logging.INFO, 
-                            format='%(asctime)s [%(module)s] %(message)s',
-                            stream=log_stream)
-        self.source = source
-        self.target = target
-        self.log_stream = log_stream
 
-    
     def run(self):
         '''
         Runs the conversion
@@ -60,7 +48,8 @@ class OggConverter:
         process = Popen(command.split(),stdout=self.log_stream)
         output, error = process.communicate()
         print error
-            
+
+
 #usage example
 if __name__ == '__main__':
     OggConverter(sys.argv[1]).run()
